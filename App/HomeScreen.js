43f88f3575ff1d818a938b129ead293053b2d028
Kelly Senna <jkellysenna@gmail.com>
2018-03-01 13:00:01 -0500

    Add header label for Home screen

--- App/HomeScreen.js
+++ App/HomeScreen.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home'
+  }
+
   render() {
     return (
       <View style={styles.container}>
