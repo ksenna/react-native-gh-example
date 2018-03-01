8fe31be2929d3d013df5c949b7595cf0718b0669
Kelly Senna <jkellysenna@gmail.com>
2018-03-01 12:58:35 -0500

    Add user profile to navigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import HomeScreen from './App/HomeScreen.js';
+import UserProfile from './App/UserProfile.js';
 
 const App = StackNavigator({
   Home: { screen: HomeScreen },
+  UserProfile: { screen: UserProfile },
 });
 
 export default App;
